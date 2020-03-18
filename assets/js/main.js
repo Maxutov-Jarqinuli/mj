8582e02a0047d4c924ae0016dc5101839c18db60
I0bSTeR <mrmaksutovakyl@gmail.com>
2020-03-19 01:24:11 +0500

    no fullpage

--- assets/js/main.js
+++ assets/js/main.js
@@ -36,4 +36,32 @@ $(document).ready(function() {
             }
         }
     });
+
+
+    try {
+        // var myFullpage = new fullpage('#fullpage', {
+        //     anchors: ['main', 'calculator'],
+        //     navigation: true,
+        //     navigationPosition: 'right',
+        //     navigationTooltips: ['Main', 'Calculator'],
+        //     responsiveWidth: 900,
+        //     scrollOverflow: true,
+        //     afterResponsive: function(isResponsive){
+        //     }
+        // });
+    } catch(e) {
+        console.log(e)
+    }
+
+    try {
+        var widgetOptions268521 = { bg_color: "transparent" }; 
+        (function() { 
+            var a = document.createElement("script"), h = "head"; 
+            a.async = true; 
+            a.src = (document.location.protocol == "https:" ? "https:" : "http:") + "//ucalc.pro/api/widget.js?id=268521&t="+Math.floor(new Date()/18e5); 
+            document.getElementsByTagName(h)[0].appendChild(a) 
+        })();
+    } catch(e) {
+        console.log(e);
+    }
 });
